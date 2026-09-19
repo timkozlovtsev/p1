8795e0290e65b40d10b3002ed7e405c25e9febc5
timkozlovtsev <agent.myxa1@gmail.com>
2026-09-19 14:05:44 +0300

    fix display

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let switchButton = document.getElementById("switch-ar-button");
 let mvContainer = document.getElementById("mv-container");
 let aFrameContainer = document.getElementById("aframe-container");
 
+mvContainer.style.display = "flex";
+aFrameContainer.style.display = "none";
+
 switchButton.addEventListener("click", () => {
     if (mvContainer.style.display !== "none") {
         mvContainer.style.display = "none";
