ecadee0d6a4a9faeb42726b36276d34e85358cc3
timkozlovtsev <agent.myxa1@gmail.com>
2026-09-19 14:41:18 +0300

    fix ui

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let switchButton = document.getElementById("switch-ar-button");
 let mvContainer = document.getElementById("mv-container");
 let aFrameContainer = document.getElementById("aframe-container");
-let aFrameUis = Array.from(document.getElementsByClassName("test"));
+let aFrameUis = Array.from(document.getElementsByClassName("mindar-ui-overlay"));
 
 mvContainer.style.display = "flex";
 aFrameContainer.style.display = "none";
