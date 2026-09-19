522b320435a9e990aa9d44cc8ea4ce6c11ba68d1
timkozlovtsev <agent.myxa1@gmail.com>
2026-09-19 14:37:02 +0300

    fix a-frame ui

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let switchButton = document.getElementById("switch-ar-button");
 let mvContainer = document.getElementById("mv-container");
 let aFrameContainer = document.getElementById("aframe-container");
+let aFrameUis = document.getElementsByClassName("test");
 
 mvContainer.style.display = "flex";
 aFrameContainer.style.display = "none";
+aFrameUis.forEach(element => {
+    element.style.display = "none";
+});
 
 switchButton.addEventListener("click", () => {
     if (mvContainer.style.display !== "none") {
         mvContainer.style.display = "none";
         aFrameContainer.style.display = "flex";
+        aFrameUis.forEach(element => {
+            element.style.display = "block";
+        });
     }
     else {
         mvContainer.style.display = "flex";
         aFrameContainer.style.display = "none";
+        aFrameUis.forEach(element => {
+            element.style.display = "none";
+        });
     }
 });
